8849771ccb8debb6a61eb8d3dd7e32501129f23b
tlx <123456@qq.com>
2017-09-27 21:27:54 +0800

    完成课时添加和编辑的弹窗共享

--- public/js/course-basic.js
+++ public/js/course-basic.js
@@ -61,7 +61,7 @@ define(['jquery','template','util','ckeditor','validate','form'],function ($,tem
 				   	dataType : 'json',
 				   	success : function(data){
 				   		if(data.code == 200){
-				   			location.href = '/course/picturen                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                       ?cs_id'+data.result.cs_id;
+				   			location.href = '/course/picture?cs_id='+data.result.cs_id;
 				   		}
 				   	}
 				   })
--- public/js/courselesson.js
+++ public/js/courselesson.js
@@ -1,4 +1,4 @@
-define(['jquery','template','util'],function ($,template,util) {
+define(['jquery','template','util','bootstrap'],function ($,template,util) {
 	//设置导航菜单选中
 	util.setMenu('/course/add');
 
@@ -14,6 +14,34 @@ define(['jquery','template','util'],function ($,template,util) {
 			// 解析数据渲染页面
 			var html = template('lessonTpl',data.result);
 			$('#lessonInfo').html(html);
+			// 绑定添加课时事件
+			$('#addLesson').click(function(){
+				var html = template('modalTpl',{operate : '添加课时'});
+				$('#modalInfo').html(html);
+				// 显示模态框
+				$('#chapterModal').modal();
+			});
+			// 绑定编辑课时事件
+			$('.editBtn').click(function(){
+				// 获取课时id
+				var ctId = $(this).attr('data-ctId');
+				// 根据课时id查询课时数据信息
+				$.ajax({
+					type : 'get',
+					url : '/api/course/chapter/edit',
+					data : {ct_id : ctId},
+					dataType : 'json',
+					success : function(data){
+						data.result.operate = '编辑课时';
+						// 解析数据，渲染模态框
+						var html = template('modalTpl',data.result);
+						$('#modalInfo').html(html);
+						// 显示模态框
+						$('#chapterModal').modal();
+
+					}
+				});
+			});
 		}
 	});
 });
